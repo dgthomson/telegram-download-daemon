4adb9411dba0f0eb467a20ac1522d7960ba26fc9
dgthomson <dgthomson@gmail.com>
2021-08-15 22:07:01 +0100

    Update telegram-download-daemon.py

--- telegram-download-daemon.py
+++ telegram-download-daemon.py
@@ -111,7 +111,7 @@ proxy = None
 def sizeof_fmt(num, suffix='B'):
     for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
         if abs(num) < 1024.0:
-            return "%3.1f%s%s" % (num, unit, suffix)
+            return "%3.2f %s%s" % (num, unit, suffix)
         num /= 1024.0
     return "%.2f %s%s" % (num, 'Yi', suffix)
 
